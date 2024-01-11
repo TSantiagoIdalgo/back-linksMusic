1f77af8f45c1b70ac9fd62ab1f3e45d9b78e6b66
TSantiagoIdalgo <130645404+TSantiagoIdalgo@users.noreply.github.com>
2024-01-11 01:15:10 -0300

    fix:(redirect to deployed app)

--- src/services/mail/template.ts
+++ src/services/mail/template.ts
@@ -12,7 +12,7 @@ export function mailTemplate (name: string, token: string) {
         <h1 style="color: #fff;">Hi 👋 <span style="color: #7648ffc7;">${name}</span></h1>
         <p style="font-size: 20px; color: #ddd;">It is great that you consider using our services, we offer the best audio quality and without any type of compression totally free</p>
         <p style="font-size: 20px; color: #ddd;">Please click on the link below to verify your email address:</p>
-        <a style="text-decoration: none; color: #fff; border: 2px solid #7648ffc7;; border-radius: 10px; padding: 15px; background: transparent; cursor: pointer; font-size: 15px;" href="http://localhost:5173/verify/?token=${token}">verify yourself now</a>
+        <a style="text-decoration: none; color: #fff; border: 2px solid #7648ffc7; border-radius: 10px; padding: 15px; background: transparent; cursor: pointer; font-size: 15px;" href="https://p-final-p-ccorp-front.vercel.app/verify/?token=${token}">verify yourself now</a>
       </div>
   </body>
   </html>
